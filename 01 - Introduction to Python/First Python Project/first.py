cc8b99be860d1218a12a49469c347a20b5cd30d3
Hossam <7osssam@protonmail.ch>
2024-12-27 17:41:23 +0200

    Added first.py and second.py files

---
+++ 01 - Introduction to Python/First Python Project/first.py
@@ -0,0 +1,7 @@
+
+print ("Name: ", "Hossam Taha")
+print ("Age: ", 25)
+print ("Country: ", "Egypt")
+print ("Summary: ", "I am a software engineer and Tester some times, I love programming and I am always looking for new challenges to improve my skills.")
+
+
