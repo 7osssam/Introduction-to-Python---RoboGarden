6d00c9fe0c6bc78eb9722635c644461cc8d89a82
Hossam <7osssam@protonmail.ch>
2024-12-28 14:38:49 +0200

    Add code to calculate age based on birth year and desired year

---
+++ 02 - Python Basics/01 - Calculate Age/main.py
@@ -0,0 +1,7 @@
+
+start_year = input("Enter your birth year: ")
+end_year = input("Enter the year you want to know your age at: ")
+
+age = int(end_year) - int(start_year)
+
+print(f"You will be {age} years old in {end_year}.")
