175dc0d006bd7a09cc9e8e3165724b74c9a0adb6
Hossam <7osssam@protonmail.ch>
2024-12-28 18:06:03 +0200

    Add factorial calculation code

---
+++ 03 -  Decision-Making and Flow Control/02 - Calculate the Factorial/main.py
@@ -0,0 +1,13 @@
+
+num = int(input("Enter a number: "))
+
+if num < 0:
+	print("Please enter a positive number")
+	exit()
+
+factorial = 1
+
+for i in range(1, num + 1):
+	factorial *= i
+
+print(f"The factorial of {num} is {factorial}")
