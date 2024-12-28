308056e2bfcbaecccf67ac017b3122b6cfc9bb05
Hossam <7osssam@protonmail.ch>
2024-12-28 15:40:57 +0200

    Refactor calculator function to handle invalid input and division by zero

--- 02 - Python Basics/02 - Calculator/main.py
+++ 02 - Python Basics/02 - Calculator/main.py
@@ -1,30 +1,27 @@
 
-def calculator() -> None:
-	num1 = int(input("Enter the first number: "))
-	num2 = int(input("Enter the second number: "))
-	operator = input("Enter the operator (+, -, *, /): ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Enter the operator (+, -, *, /): ")
 
-	if not num1.isdigit() or not num2.isdigit():
-		print("Please enter a valid number")
-		return
+if not num1.isdigit() or not num2.isdigit():
+	print("Please enter a valid number")
+	exit()
 
-	if operator not in ["+", "-", "*", "/"]:
-		print("Please enter a valid operator")
-		return
+if operator not in ["+", "-", "*", "/"]:
+	print("Please enter a valid operator")
+	exit()
 
-	if operator == "+":
-		result = num1 + num2
-	elif operator == "-":
-		result = num1 - num2
-	elif operator == "*":
-		result = num1 * num2
-	elif operator == "/":
-		if num2 == 0:
-			print("Cannot divide by zero")
-			return
-		result = num1 / num2
+if operator == "+":
+	result = num1 + num2
+elif operator == "-":
+	result = num1 - num2
+elif operator == "*":
+	result = num1 * num2
+elif operator == "/":
+	if num2 == 0:
+		print("Cannot divide by zero")
+		exit()
+	result = num1 / num2
 
-	print(f"The result of {num1} {operator} {num2} is {result}")
+print(f"The result of {num1} {operator} {num2} is {result}")
 
-
-calculator()
